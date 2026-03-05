35b9c843b5de16ee7b617ef7fe11d990b6e20e88
郁涵 <149996560+wjoe14@users.noreply.github.com>
2026-03-06 02:42:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import requests
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(page_title="AI華語拍檔-人工評閱華語教材語篇品質評分平台", layout="wide")
 
 st.set_page_config(
     page_title="AI華語拍檔-人工評閱華語教材語篇品質評分平台",
